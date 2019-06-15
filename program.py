4b62e4d1874f7592b38a6027d0517bf4be56e586
IrinKalkatina <50328338+IrinKalkatina@users.noreply.github.com>
2019-06-15 13:17:01 +0200

    Update program.py

--- program.py
+++ program.py
@@ -1,13 +1,13 @@
 class Solution:
     J = input("Give distinct characters of jewels: ")
     S = input("Give all characters of stones you have: ")
-    counter = 0
+    counterJewels = 0
     def numJewelsInStones(self):
         for i in range(len(self.S)):
             for j in range(len(self.J)):
                 if self.S[i] == self.J[j]:
-                    self.counter += 1
-        print(self.counter)
+                    self.counterJewels += 1
+        return self.counterJewels
 
 
 ##     ##     ##     ##     ##     ##     ##     ##     ##     ##     
